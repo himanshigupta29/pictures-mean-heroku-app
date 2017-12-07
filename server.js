d7deb37fb58ed61f1a06e57d4c67c8c5fb7725ee
himanshigupta <himanshigupta29@gmail.com>
2017-12-07 17:00:09 +0530

    key fix

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-const route = require('./routes/route');
-mongoose.connect('mongodb://himanshigupta:himanshi222@ds129936.mlab.com:29936/picture-gallery');
+// const route = require('./routes/route');
 
-// mongodb://<dbuser>:<dbpassword>@ds129936.mlab.com:29936/picture-gallery
+const MONGO_USER = process.env.MLAB_USER || "XXX-XXX-XXX";
+const MONGO_USER_SECRET = process.env.MLAB_USER_SECRET || "XXX-XXX-XXX";
+
+const mongoURL = 'mongodb://' + MONGO_USER + ':' + MONGO_USER_SECRET + '@ds129936.mlab.com:29936/picture-gallery';
+
+mongoose.connect(mongoURL);
 
 mongoose.connection.on('connected', () => {
   console.log('connected to mongodb');
 });
-mongoose.connection.on('error', () => {
+mongoose.connection.on('error', (error) => {
   console.log("error in connection wth mongo", error);
 });
 
